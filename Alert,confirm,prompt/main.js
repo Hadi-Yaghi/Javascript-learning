793dc3c92a0e28f2491c853232d4bae23a4770dc
Hadi Yaghi <12330383@students.liu.edu.lb>
2026-07-11 14:42:53 +0300

    Add Events notes

---
+++ Alert,confirm,prompt/main.js
@@ -0,0 +1,13 @@
+alert("Test");
+console.log("test")
+let msg = confirm("are you sure");
+if(confirm === true){
+    console.log("item deleted");
+
+}
+else{
+    console.log("item not deleted")
+}
+
+
+let promptMsg = prompt("suitable day for you");
